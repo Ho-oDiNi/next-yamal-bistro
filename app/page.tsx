f05ca70d64932eda97f21b3f257a4afb0faa1fab
Ho-oDiNi <Evgen5060@gmail.com>
2025-06-12 18:35:57 +0700

    refactor(css): уменьшаем css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import ContactSection from "@/app/_components/ContactSection";
 import HomeSection from "@/app/_components/HomeSection/HomeSection";
 import KitchenMenuSection from "@/app/_components/KitchenMenuSection/KitchenMenuSection";
@@ -9,8 +11,24 @@ export default function page() {
     return (
         <main>
             <HomeSection />
-            <SeoSection />
-            <KitchenMenuSection />
+            <div className="relative overflow-hidden">
+                <Image
+                    className="animate-spin-slow absolute -right-2/5 z-1"
+                    src="/icons/deer_antlers_1024.svg"
+                    alt=""
+                    height={1024}
+                    width={1024}
+                />
+                <Image
+                    className="animate-reverse-spin absolute top-[17%] -right-[34%] z-1"
+                    src="/icons/deer_antlers_824.svg"
+                    alt=""
+                    height={824}
+                    width={824}
+                />
+                <SeoSection />
+                <KitchenMenuSection />
+            </div>
             <ReservationSection />
             <ReviewsSection />
             <ContactSection />
