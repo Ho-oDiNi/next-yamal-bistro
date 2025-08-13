297df4d197b40cbfe0d730531dc597b1bef00bc6
Ho-oDiNi <Evgen5060@gmail.com>
2025-08-13 07:28:42 +0500

    refactor(css): рефактор стилей

--- components/shared/Header/Header.tsx
+++ components/shared/Header/Header.tsx
@@ -7,7 +7,7 @@ import HeaderNavbar from "./HeaderNavbar";
 const Header = () => {
     return (
         <header className="bg-opacity-10 sticky -top-[0px] z-99 bg-white/90 py-5 text-xl font-bold text-(--color-brand-dark) backdrop-blur-md">
-            <div className="mx-auto flex max-w-5xl flex-row items-center justify-between">
+            <div className="container mx-auto flex max-w-5xl flex-row items-center justify-between">
                 <Link href="#">
                     <Image
                         className="pb-[8px]"
