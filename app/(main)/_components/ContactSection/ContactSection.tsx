a31927771e2b05bee3eef5c59684db1e0065b68d
Ho_oDiNi <Evgen5060@gmail.com>
2025-08-14 12:52:23 +0000

    feat(верстка): адаптив под мобильные

--- app/(main)/_components/ContactSection/ContactSection.tsx
+++ app/(main)/_components/ContactSection/ContactSection.tsx
@@ -11,92 +11,100 @@ const ContactSection = () => {
             <div className="mx-auto h-auto w-full max-w-420 rounded-[26px] bg-white p-(--space-intside-container)">
                 <div className="container mx-auto">
                     <h2>Наши контакты</h2>
-                    <div className="flex justify-between not-italic">
-                        <div className="max-w-[26%] flex-1">
-                            <div>
-                                <h3 className="my-5 text-2xl font-semibold">
-                                    Часы работы
-                                </h3>
-                                <WorkTime
-                                    weekDay="Пн - Чт"
-                                    timeOpen="7:30"
-                                    timeClose="21:00"
-                                />
-                                <WorkTime
-                                    weekDay="Пт"
-                                    timeOpen="7:30"
-                                    timeClose="22:00"
-                                />
-                                <WorkTime
-                                    weekDay="Сб"
-                                    timeOpen="9:00"
-                                    timeClose="22:00"
-                                />
-                                <WorkTime
-                                    weekDay="Вс"
-                                    timeOpen="9:00"
-                                    timeClose="21:00"
-                                />
-                            </div>
-                            <div className="flex flex-col">
-                                <h3 className="my-3 text-2xl font-semibold">
-                                    Телефон
-                                </h3>
-                                <a href="tel:+79044755099" className="my-[7px]">
-                                    +7 (904) 475-50-99
-                                </a>
-                                <a href="tel:+79044755099" className="my-[7px]">
-                                    +7 (904) 475-50-99
-                                </a>
-                            </div>
-                        </div>
-                        <div className="max-w-[26%] flex-1">
-                            <div>
-                                <h3 className="my-5 text-2xl font-semibold">
-                                    Адрес
-                                </h3>
-                                <span>г. Салехард, </span>
-                                <span>ул. Свердлова, </span>
-                                <span>д. 34А</span>
-                            </div>
-
-                            <div>
-                                <h3 className="my-5 text-2xl font-semibold">
-                                    Почта
-                                </h3>
-                                <a href="mailto:yamdstone@mail.ru">
-                                    yamdstone@mail.ru
-                                </a>
-                            </div>
-
-                            <div>
-                                <h3 className="my-5 text-2xl font-semibold">
-                                    Социальные сети
-                                </h3>
-                                <div className="ml-[26px] flex gap-4">
-                                    <a href="https://t.me/YamalBistro">
-                                        <Image
-                                            src="/icons/tg.svg"
-                                            height={40}
-                                            width={40}
-                                            alt=""
-                                        />
+                    <div className="flex flex-col justify-between not-italic lg:flex-row">
+                        <div className="flex justify-between not-italic lg:max-w-[52%]">
+                            <div className="max-w-[50%] flex-1">
+                                <div>
+                                    <h3 className="my-5 text-2xl font-semibold">
+                                        Часы работы
+                                    </h3>
+                                    <WorkTime
+                                        weekDay="Пн - Чт"
+                                        timeOpen="7:30"
+                                        timeClose="21:00"
+                                    />
+                                    <WorkTime
+                                        weekDay="Пт"
+                                        timeOpen="7:30"
+                                        timeClose="22:00"
+                                    />
+                                    <WorkTime
+                                        weekDay="Сб"
+                                        timeOpen="9:00"
+                                        timeClose="22:00"
+                                    />
+                                    <WorkTime
+                                        weekDay="Вс"
+                                        timeOpen="9:00"
+                                        timeClose="21:00"
+                                    />
+                                </div>
+                                <div className="flex flex-col">
+                                    <h3 className="my-3 text-2xl font-semibold">
+                                        Телефон
+                                    </h3>
+                                    <a
+                                        href="tel:+79044755099"
+                                        className="my-[7px]"
+                                    >
+                                        +7 (904) 475-50-99
                                     </a>
-                                    <a href="https://vk.com/bistroyamal">
-                                        <Image
-                                            src="/icons/vk.svg"
-                                            height={40}
-                                            width={40}
-                                            alt=""
-                                        />
+                                    <a
+                                        href="tel:+79044755099"
+                                        className="my-[7px]"
+                                    >
+                                        +7 (904) 475-50-99
                                     </a>
+                                </div>
+                            </div>
+                            <div className="max-w-[50%] flex-1">
+                                <div>
+                                    <h3 className="my-5 text-2xl font-semibold">
+                                        Адрес
+                                    </h3>
+                                    <span>г. Салехард, </span>
+                                    <span>ул. Свердлова, </span>
+                                    <span>д. 34А</span>
+                                </div>
+
+                                <div>
+                                    <h3 className="my-5 text-2xl font-semibold">
+                                        Почта
+                                    </h3>
+                                    <a href="mailto:yamdstone@mail.ru">
+                                        yamdstone@mail.ru
+                                    </a>
+                                </div>
+
+                                <div>
+                                    <h3 className="my-5 text-2xl font-semibold">
+                                        Социальные сети
+                                    </h3>
+                                    <div className="ml-[26px] flex gap-4">
+                                        <a href="https://t.me/YamalBistro">
+                                            <Image
+                                                src="/icons/tg.svg"
+                                                height={40}
+                                                width={40}
+                                                alt=""
+                                            />
+                                        </a>
+                                        <a href="https://vk.com/bistroyamal">
+                                            <Image
+                                                src="/icons/vk.svg"
+                                                height={40}
+                                                width={40}
+                                                alt=""
+                                            />
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <iframe
                             sandbox="allow-orientation-lock allow-scripts allow-same-origin allow-popups"
                             src="https://yandex.ru/map-widget/v1/?from=api-maps&ll=66.600484%2C66.526556&mode=search&oid=95926227579&ol=biz&origin=jsapi_2_1_79&z=16.79"
-                            className="ml-[35px] h-[320px] w-[385px] flex-1 rounded-[26px] shadow-md"
+                            className="mt-8 ml-8 h-[320px] w-[385px] rounded-[26px] shadow-md lg:flex-1"
                             title="Ямал Бистро на карте Салехарда"
                         />
                     </div>
--- app/(main)/_components/SeoSection/AdvantageElement.tsx
+++ app/(main)/_components/SeoSection/AdvantageElement.tsx
@@ -9,17 +9,21 @@ interface AdvantageElementProps {
 
 const AdvantageElement = (props: AdvantageElementProps) => {
     return (
-        <div className="flex">
+        <div className="flex items-center">
             <Image
                 src={`/icons/${props.icon}.svg`}
-                className="mr-8"
+                className="mr-4 lg:mr-8"
                 height={24}
                 width={24}
                 alt=""
             />
             <div>
-                <dt className="text-2xl font-semibold">{props.title}</dt>
-                <dd className="text-base font-light">{props.description}</dd>
+                <dt className="text-lg font-semibold lg:text-2xl">
+                    {props.title}
+                </dt>
+                <dd className="text-sm font-light lg:text-base">
+                    {props.description}
+                </dd>
             </div>
         </div>
     );
