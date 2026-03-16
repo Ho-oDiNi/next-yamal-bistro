c9b96a9dbede976655bf45a8764532fe877bc9fe
Evgeny Irikov <Evgen5060@gmail.com>
2026-03-16 13:17:44 +0700

    feat(admin-sidebar): добавить админ-редактор блюд и категорий

--- src/shared/ui/StyledInput/index.ts
+++ src/shared/ui/StyledInput/index.ts
@@ -1,3 +1,4 @@
 export { StyledInput } from "./ui/StyledInput";
+export { StyledTextarea } from "./ui/StyledTextarea";
 export { TimeInput } from "./ui/TimeInput";
 export { DateInput } from "./ui/DateInput";
