2194e9c0a9554a3ced385383e7bf7ea5386418f9
Ho-oDiNi <Evgen5060@gmail.com>
2026-03-17 16:33:54 +0700

    fix: исправил изображение резерва

--- src/widgets/lead-reservation/ui/ReservationContainer.tsx
+++ src/widgets/lead-reservation/ui/ReservationContainer.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 
-import reservationImg from "@images/reservation.png";
+import reservationImg from "@images/reservation_desctop.png";
 
 type ReservationContainerProps = {
     children: ReactNode;
@@ -13,7 +13,7 @@ export const ReservationContainer = ({
     children,
 }: ReservationContainerProps) => {
     return (
-        <section className="flex-center relative bg-[url('/images/reservation.png')] bg-cover bg-center bg-no-repeat text-white md:bg-none">
+        <section className="flex-center xs:bg-[url('/images/reservation_laptop.png')] relative bg-[url('/images/reservation_mobile.png')] bg-cover bg-center bg-no-repeat text-white md:bg-none">
             <Image
                 className="bottom-0 z-1 hidden h-full w-full object-cover md:block"
                 src={reservationImg}
