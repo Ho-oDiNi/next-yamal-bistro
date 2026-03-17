98b844c39e6e41e83abaf0e2f365773b020a59cf
Ho-oDiNi <Evgen5060@gmail.com>
2026-03-17 17:02:54 +0700

    feat: добавил 404 страницу

--- src/pages/not-found/ui/NotFoundPage.tsx
+++ src/pages/not-found/ui/NotFoundPage.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 
 import { StyledLink } from "@/shared/ui/StyledLink";
-import { StyledSection } from "@/shared/ui/StyledSection";
 
-import notFoundImage from "@images/404.gif";
+import notFoundImage from "@images/404.png";
 
 export const NotFoundPage = () => {
     return (
-        <StyledSection>
-            <div className="flex-center flex-col-reverse gap-6 md:flex-row">
+        <section className="m-auto max-w-4xl px-4 py-12 md:py-6 xl:py-16">
+            <div className="flex-between container m-auto w-full flex-col-reverse gap-12 md:flex-row">
                 <div className="flex flex-col gap-8 md:gap-12">
-                    <hgroup>
+                    <hgroup className="space-y-4">
                         <h1 className="text-h1">Страница не найдена</h1>
-                        <p className="text-main">Но есть много других</p>
+                        <p className="text-accent">Но есть много других</p>
                     </hgroup>
                     <StyledLink href="/" variant="primary" size="lg">
                         На главную
@@ -21,9 +20,9 @@ export const NotFoundPage = () => {
                 <Image
                     src={notFoundImage}
                     alt="Ошибка 404"
-                    className="h-auto w-100"
+                    className="h-auto w-100 rounded-2xl"
                 />
             </div>
-        </StyledSection>
+        </section>
     );
 };
