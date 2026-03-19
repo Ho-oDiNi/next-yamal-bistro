c47a54918ac1bec2e436685544614422687b5520
Ho-oDiNi <Evgen5060@gmail.com>
2026-03-19 18:46:02 +0700

    feat: добавил вебмастер

--- src/pages/home/config/index.ts
+++ src/pages/home/config/index.ts
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
     verification: {
-        yandex: "108153196",
+        yandex: "4e2e707416432fc3",
     },
     title: "Кафе северной кухни в Салехарде | Ямал Бистро",
     description:
